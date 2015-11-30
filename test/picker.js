c2ff45c261c15fde382a96afb47b2cfc52ef1e05
Luke Karrys <lukekarrys@gmail.com>
2015-11-29 18:12:21 -0700

    implicit

--- test/picker.js
+++ test/picker.js
@@ -7,31 +7,29 @@ import {participants} from 'getconfig';
 
 const ITERATIONS = 2500;
 
-times(ITERATIONS).forEach((n) => {
-  test(`Iteration ${n}`, (t) => {
-    const picked = picker(participants);
-    const names = compact(pluck(picked, 'name'));
-    const recipients = compact(pluck(picked, 'recipient'));
+times(ITERATIONS).forEach((n) => test(`Iteration ${n}`, (t) => {
+  const picked = picker(participants);
+  const names = compact(pluck(picked, 'name'));
+  const recipients = compact(pluck(picked, 'recipient'));
 
-    t.ok(names.length, 'Names has items');
-    t.ok(recipients.length, 'Recipients has items');
-    t.equal(names.length, uniq(names).length, 'Names are unique');
-    t.equal(recipients.length, uniq(recipients).length, 'Recipients are unique');
-    t.equal(names.length, recipients.length, 'Names and recipients are equal lengths');
+  t.ok(names.length, 'Names has items');
+  t.ok(recipients.length, 'Recipients has items');
+  t.equal(names.length, uniq(names).length, 'Names are unique');
+  t.equal(recipients.length, uniq(recipients).length, 'Recipients are unique');
+  t.equal(names.length, recipients.length, 'Names and recipients are equal lengths');
 
-    each(picked, (participant) => {
-      t.ok(participant.recipient, 'Recipient exists');
-      t.ok(participant.name, 'Name exists');
-      t.ok(participant.skip, 'Skip exists');
+  each(picked, (participant) => {
+    t.ok(participant.recipient, 'Recipient exists');
+    t.ok(participant.name, 'Name exists');
+    t.ok(participant.skip, 'Skip exists');
 
-      t.equal(typeof participant.recipient, 'string', 'Recipient is a string');
-      t.equal(typeof participant.name, 'string', 'Name is a stirng');
-      t.ok(Array.isArray(participant.skip), 'Skip is an array');
+    t.equal(typeof participant.recipient, 'string', 'Recipient is a string');
+    t.equal(typeof participant.name, 'string', 'Name is a stirng');
+    t.ok(Array.isArray(participant.skip), 'Skip is an array');
 
-      t.notOk(participant.recipient === participant.name, 'Recipient is not the participant');
-      t.notOk(contains(participant.skip, participant.recipient), 'Recipient is not in skip');
-    });
-
-    t.end();
+    t.notOk(participant.recipient === participant.name, 'Recipient is not the participant');
+    t.notOk(contains(participant.skip, participant.recipient), 'Recipient is not in skip');
   });
-});
+
+  t.end();
+}));
